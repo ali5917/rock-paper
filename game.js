fb6c0cf0abbc03e4d5b894e0f0321bc158b62681
Ali Kashif <alikashif5917@gmail.com>
2023-08-03 21:18:07 +0500

    added start button in index.html and update game.js

--- game.js
+++ game.js
@@ -1,77 +1,88 @@
-function getUserChoice() {
-    let userChoice = prompt("Enter your choice: ");
-    userChoice = userChoice.toLowerCase();
-    return userChoice;
-}
 
-function getComputerChoice() {
-    const choices = ["rock", "paper", "scissor"]
-    let randnum = Math.floor(Math.random() * 3);
-    return choices[randnum];
-}
+document.addEventListener("DOMContentLoaded", function() {
+    let btn = document.getElementById("btn")
 
-function round(compChoice, userChoice) {
-    if (compChoice === userChoice) {
-        return "Match drew!";
-    }
-    else if (compChoice === 'rock' && userChoice === "paper") {
-        return "You Won! Paper beats Rock :)"
-    }
-    else if (compChoice === 'rock' && userChoice === "scissor") {
-        return "You Lost! Rock beats Scissor :("
-    }
-    else if (compChoice === 'paper' && userChoice === "scissor") {
-        return "You Won! Scissor beats Paper :)"
-    }
-    else if (compChoice === 'scissor' && userChoice === "rock") {
-        return "You Won! Rock beats Scissor :)"
-    }
-    else if (compChoice === 'paper' && userChoice === "rock") {
-        return "You Lost! Paper beats Rock :("
-    }
-    else if (compChoice === 'scissor' && userChoice === "paper") {
-        return "You Lost! Scissor beats Paper :("
-    }  
-}
-
-function game () {
-    let draws = 0;
-    let userPoints = 0;
-    let compPoints = 0;
-    let roundnum = 1;
-    for (let i = 0; i < 5; i++) {
-        let compChoice = getComputerChoice();
-        const userChoice = getUserChoice();
-        const winner = round(compChoice, userChoice);
-        console.log("ROUND " + roundnum);
-        console.log("Your choice was " + userChoice);
-        console.log("Opponent's choice was " + compChoice);
-        console.log(winner);
-        console.log("");
-
-        if (winner.includes("You Won")) {
-            userPoints++;
-        } 
-        else if (winner.includes("You Lost")) { 
-            compPoints++;
+    btn.addEventListener("click", function(event) {
+        event.preventDefault();
+        function getUserChoice() {
+            let userChoice = prompt("Enter your choice (Rock, Paper or Scissor): ");
+            userChoice = userChoice.toLowerCase();
+            return userChoice;
         }
-        else {
-            draws++;
+
+        function getComputerChoice() {
+            const choices = ["rock", "paper", "scissor"]
+            let randnum = Math.floor(Math.random() * 3);
+            return choices[randnum];
         }
-        roundnum++;
-    }
-    console.log("GAME OVER!")
-    if (userPoints > compPoints) {
-        console.log("Congrats! You won the game.")
-    }
-    else if (userPoints < compPoints) {
-        console.log("Sorry, You lost the game.")
-    }
-    else {
-        console.log("Game drew, Try Again")
-    }
-    console.log("Your Points: " + userPoints);
-    console.log("Opponent Points: " + compPoints);
-    console.log("Draws: " + draws);
-}
-game();
+
+        function round(compChoice, userChoice) {
+            if (compChoice === userChoice) {
+            return "Match drew!";
+            }
+            else if (compChoice === 'rock' && userChoice === "paper") {
+            return "You Won! Paper beats Rock :)"
+            }
+            else if (compChoice === 'rock' && userChoice === "scissor") {
+            return "You Lost! Rock beats Scissor :("
+            }
+            else if (compChoice === 'paper' && userChoice === "scissor") {
+            return "You Won! Scissor beats Paper :)"
+            }
+            else if (compChoice === 'scissor' && userChoice === "rock") {
+            return "You Won! Rock beats Scissor :)"
+            }
+             else if (compChoice === 'paper' && userChoice === "rock") {
+            return "You Lost! Paper beats Rock :("
+            }
+            else if (compChoice === 'scissor' && userChoice === "paper") {
+            return "You Lost! Scissor beats Paper :("
+            }  
+        }
+
+        function game () {
+            let draws = 0;
+            let userPoints = 0;
+            let compPoints = 0;
+            let roundnum = 1;
+            for (let i = 0; i < 5; i++) {
+                let compChoice = getComputerChoice();
+                const userChoice = getUserChoice();
+                const winner = round(compChoice, userChoice);
+                console.log("ROUND " + roundnum + "/5");
+                console.log("Your choice was " + userChoice);
+                console.log("Opponent's choice was " + compChoice);
+                console.log(winner);
+                console.log("");
+
+                if (winner.includes("You Won")) {
+                    userPoints++;
+                } 
+                else if (winner.includes("You Lost")) { 
+                    compPoints++;
+                }
+                else {
+                    draws++;
+                }
+                roundnum++;
+            }
+            console.log("GAME OVER!")
+            if (userPoints > compPoints) {
+                console.log("Congrats! You WON the game.")
+            }
+            else if (userPoints < compPoints) {
+                console.log("Sorry, You LOST the game.")
+            }
+            else {
+                console.log("Game drew, Try Again")
+            }
+            console.log("Your Points: " + userPoints);
+            console.log("Opponent Points: " + compPoints);
+            console.log("Draws: " + draws);
+        }
+        game();
+
+
+
+    })
+})
